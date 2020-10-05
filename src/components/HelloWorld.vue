05094a9ca72beb61effbfe2c511254d552450b75
姚霖宏 <7881785+ylh1995@user.noreply.gitee.com>
2020-10-05 14:24:57 +0800

    leancloud-体验

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,37 +1,35 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+   fuck
   </div>
 </template>
 
 <script>
+import AV from '@/lib/serverless'
+import Vue from 'vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+Vue.use(VueAxios, axios);
+const TestObject = AV.Object.extend('TestObject');
+
+console.log(typeof (TestObject))
+
+let newName = new TestObject;
+console.log(newName)
+console.log('fuck')
+newName.set('words', 'Hello world!');
+console.log('fuck')
+newName.save().then((newnName) => {
+  console.log('保存成功。')
+})
+
+axios.get()
+
 export default {
+data(){
+  return AV
+},
   name: 'HelloWorld',
   props: {
     msg: String
---
+++ src/lib/serverless.vue
@@ -0,0 +1,15 @@
+
+<script>
+
+  const AV = require('leancloud-storage');
+  //onst { Query, User } = AV;
+  AV.init({
+    appId: "uSEu03f3fyUarpUzrhrNLsfV-gzGzoHsz",
+    appKey: "WOFNaGh2szjxD0g7LK7bx1K9",
+    serverURL: "https://useu03f3.lc-cn-n1-shared.com"
+  });
+
+  export  default AV
+
+</script>
+
